fd35ae8e0f508da9254c9b62a07e3453c85f6247
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-05-13 19:35:26 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -51,13 +51,13 @@ struct Stats compute_statistics(const float* numberset, int setlength)
     
 }
 
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+alerter_funcptr check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
     (*alerters[0])(maxThreshold,computedStats);
     (*alerters[1])(maxThreshold,computedStats);
 }
 
-void emailAlerter(float maxThreshold, struct Stats computedStats)
+alerter_funcptr emailAlerter(float maxThreshold, struct Stats computedStats)
 {
     if(computedStats.max > maxThreshold)
     {
