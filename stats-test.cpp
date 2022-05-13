c5a52b4435e2ed20b9d8a28e82f0e9e938c6bb79
Jegan030897 <105204506+Jegan030897@users.noreply.github.com>
2022-05-13 22:51:26 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -19,9 +19,7 @@ TEST_CASE("reports average, minimum and maximum") {
 TEST_CASE("average is NaN for empty array") {
     struct Stats computedStats = compute_statistics(0, 0);
     
-    REQUIRE(isnan(computedStats.average));
-    REQUIRE(isnan(computedStats.max));
-    REQUIRE(isnan(computedStats.min));
+    REQUIRE(isnan(computedStats.average) != 0);
 }
 
 TEST_CASE("raises alerts when max is greater than threshold") {
